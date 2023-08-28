05a4992bf8ba901b0b6f9bc2de1d8ac9d9453829
lalalla <>
2023-08-27 22:33:41 -0700

    second commit

--- connection.js
+++ connection.js
@@ -1,9 +1,12 @@
 const mysql = require('mysql')
+const dotenv = require("dotenv")
+
+dotenv.config()
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'test'
+    host:process.env.host,
+    user: process.env.user,
+    password:process.env.password,
+    database: process.env.database
   })
 
   module.exports ={
